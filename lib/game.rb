f84d87475153591e37940638e68ed446644350d1
Chris Romero <chrisleeromero1@gmail.com>
2022-02-07 19:32:12 -0700

    Feat: add and test board

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,16 @@
 require 'pry'
 
 class Game
+attr_reader :board
+
+  def initialize
+    @board = "ABCDEFG\n.......\n.......\n.......\n.......\n.......\n......."
+  end
 
   def start
-    p "Welcome to connect 4!"
+    p "Welcome to connect 4!\n#{@board}"
+    # p @board
   end
+
 
 end
--- spec/start_spec.rb
+++ spec/start_spec.rb
@@ -1,22 +1,29 @@
 require 'rspec'
 require './lib/game.rb'
+require 'pry'
 
 describe Game do
 
-  describe 'Initialize' do
+  describe '# Initialize' do
     it "exists" do
       game = Game.new
       expect(game).to be_an_instance_of(Game)
     end
 
-  end
-
-  describe 'Start' do
-    it '# can print a welcome message' do
+    it "can print an empty game board" do
       game = Game.new
-      expect(game.start).to eq("Welcome to connect 4!")
+      expect(game.board).to eq("ABCDEFG\n.......\n.......\n.......\n.......\n.......\n.......")
     end
   end
+
+  describe '# Start' do
+    it 'can print a welcome message' do
+      game = Game.new
+      expect(game.start).to eq("Welcome to connect 4!\nABCDEFG\n.......\n.......\n.......\n.......\n.......\n.......")
+
+    end
+  end
+
 
 
 end
