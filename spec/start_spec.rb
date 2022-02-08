eb63f84c1ef236040491d75e98ef910363cd642e
Chris Romero <chrisleeromero1@gmail.com>
2022-02-07 19:00:51 -0700

    Test: add initialize test

--- spec/start_spec.rb
+++ spec/start_spec.rb
@@ -2,10 +2,21 @@ require 'rspec'
 require './lib/game.rb'
 
 describe Game do
+
+  describe 'Initialize' do
+    it "exists" do
+      game = Game.new
+      expect(game).to be_an_instance_of(Game)
+    end
+
+  end
+
   describe 'Start' do
     it '# can print a welcome message' do
       game = Game.new
       expect(game.start).to eq("Welcome to connect 4!")
     end
   end
+
+
 end
