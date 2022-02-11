51ca226003db8c8e669e8923f73820f6c8b1485f
Chris Romero <chrisleeromero1@gmail.com>
2022-02-11 00:11:39 -0700

    Feat: update player class with vertical wins method

--- lib/board.rb
+++ lib/board.rb
@@ -9,7 +9,11 @@ attr_reader :board, :player1, :player2
     @player2 = player2
     @board = {
       :row0 => ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-      :row1 => [Space.new('a1'), Space.new('b1'), Space.new('c1'), Space.new('d1'), Space.new('e1'), Space.new('f1'), Space.new('g1')],
+      :row1 => [
+        Space.new('a1'), Space.new('b1'),
+        Space.new('c1'), Space.new('d1'),
+        Space.new('e1'), Space.new('f1'),
+        Space.new('g1')],
       :row2 => [Space.new('a2'), Space.new('b2'), Space.new('c2'), Space.new('d2'), Space.new('e2'), Space.new('f2'), Space.new('g2')],
       :row3 => [Space.new('a3'), Space.new('b3'), Space.new('c3'), Space.new('d3'), Space.new('e3'), Space.new('f3'), Space.new('g3')],
       :row4 => [Space.new('a4'), Space.new('b4'), Space.new('c4'), Space.new('d4'), Space.new('e4'), Space.new('f4'), Space.new('g4')],
@@ -29,9 +33,20 @@ attr_reader :board, :player1, :player2
     end
   end
 
-  def print_welcome
-    puts "Welcome to connect 4!"
-    puts "Select a column (ABCDEFG)"
+  def menu
+    puts "Welcome to CONNECT FOUR!"
     puts " "
+    puts "Enter p to play. Enter q to quit."
+    answer = gets.chomp
+    answer
+  end
+
+  def print_welcome
+    if menu == 'p'
+      puts "Select a column (ABCDEFG)"
+      puts " "
+    else
+      puts "See you later!"
+    end
   end
 end
