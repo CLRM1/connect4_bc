76cbafab55e073e3fd318e7bb407f746d965808f
Chris Romero <chrisleeromero1@gmail.com>
2022-02-11 19:39:24 -0700

    Feat: update runner file to return to main menu

--- runner.rb
+++ runner.rb
@@ -27,12 +27,29 @@ if board.menu == 'p'
     puts "Round:#{turn_counter}"
     puts " "
     board.print_board
-
   end
+
+  player1.has_won = false
+  puts "Welcome to Connect 4!"
+  puts " "
+  board.menu
+  board.print_board
+
+  until player1.has_won? || computer.has_won? do
+
+    player1.add_piece(gets.chomp)
+    computer.add_piece
+
+    turn_counter += 1
+    puts " "
+    puts "Round:#{turn_counter}"
+    puts " "
+    board.print_board
+  end
+
   5.times do
     puts " "
   end
+  puts "See you later!"
+
 end
-
-
-puts "See you later!"
