91369de596bb15cb8cdaab63fd2678dc66b7180f
Chris Romero <chrisleeromero1@gmail.com>
2022-02-11 20:07:45 -0700

    Feat: update runner file with refactor

--- runner.rb
+++ runner.rb
@@ -29,12 +29,26 @@ if board.menu == 'p'
     board.print_board
   end
 
-  player1.has_won = false
-  puts "Welcome to Connect 4!"
-  puts " "
-  board.menu
-  board.print_board
+  5.times do
+    puts " "
+  end
 
+end
+
+
+board2 = Board.new
+
+player1 = Player.new(board2)
+
+computer = Computer.new(board2)
+
+turn_counter = 0
+puts "Welcome to Connect 4!"
+puts " "
+if board2.menu == 'p'
+  puts " "
+  puts "Choose a column (ABCDEFG)"
+  board2.print_board
   until player1.has_won? || computer.has_won? do
 
     player1.add_piece(gets.chomp)
@@ -44,12 +58,11 @@ if board.menu == 'p'
     puts " "
     puts "Round:#{turn_counter}"
     puts " "
-    board.print_board
+    board2.print_board
   end
 
   5.times do
     puts " "
   end
-  puts "See you later!"
 
 end
