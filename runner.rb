92ff427c4c88c1fcb4530209e07061bd71e632d5
Chris Romero <chrisleeromero1@gmail.com>
2022-02-11 20:24:38 -0700

    Feat: refactor runner file to play or quit on a loop

--- runner.rb
+++ runner.rb
@@ -4,65 +4,42 @@ require './lib/space'
 require './lib/computer'
 require 'pry'
 
-board = Board.new
+game_count = 0
+answer = "p or q"
+while game_count >= 0 &&
+  board = Board.new
 
-player1 = Player.new(board)
+  player1 = Player.new(board)
 
-computer = Computer.new(board)
+  computer = Computer.new(board)
 
-turn_counter = 0
-puts "Welcome to Connect 4!"
-puts " "
-if board.menu == 'p'
+  turn_counter = 0
+  puts "Welcome to Connect 4!"
   puts " "
-  puts "Choose a column (ABCDEFG)"
-  board.print_board
-  until player1.has_won? || computer.has_won? do
-
-    player1.add_piece(gets.chomp)
-    computer.add_piece
-
-    turn_counter += 1
+  answer = board.menu
+  if answer.upcase == 'P'
     puts " "
-    puts "Round:#{turn_counter}"
-    puts " "
+    puts "Choose a column (ABCDEFG)"
     board.print_board
-  end
+    until player1.has_won? || computer.has_won? do
 
-  5.times do
-    puts " "
-  end
+      player1.add_piece(gets.chomp)
+      computer.add_piece
 
-end
+      turn_counter += 1
+      puts " "
+      puts "Round:#{turn_counter}"
+      puts " "
+      board.print_board
+    end
 
-
-board2 = Board.new
-
-player1 = Player.new(board2)
-
-computer = Computer.new(board2)
-
-turn_counter = 0
-puts "Welcome to Connect 4!"
-puts " "
-if board2.menu == 'p'
-  puts " "
-  puts "Choose a column (ABCDEFG)"
-  board2.print_board
-  until player1.has_won? || computer.has_won? do
-
-    player1.add_piece(gets.chomp)
-    computer.add_piece
-
-    turn_counter += 1
-    puts " "
-    puts "Round:#{turn_counter}"
-    puts " "
-    board2.print_board
-  end
-
-  5.times do
-    puts " "
+    5.times do
+      puts " "
+    end
+    game_count += 1
+  elsif answer.upcase == 'Q'
+    puts "See you later!"
+    break
   end
 
 end
