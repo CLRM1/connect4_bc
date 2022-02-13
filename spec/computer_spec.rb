98ee07737869515494b3b6dbfc078c0e668ff4f8
Chris Romero <chrisleeromero1@gmail.com>
2022-02-13 15:37:41 -0700

    Test: computer_spec tests all methods

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -17,4 +17,27 @@ describe Computer do
     expect(computer.has_won).to be(false)
   end
 
+  it "can add a piece randomly" do
+    board = Board.new
+    computer = Computer.new(board)
+    10.times do
+      computer.add_piece
+    end
+    symbols = []
+    board.rows.drop(1).each do |rows, spaces|
+      symbols << spaces.map do |space|
+        space.symbol
+      end
+    end
+    symbols.flatten!
+    expect(symbols.all?('.')).to be(false)
+    o_symbols = 0
+    symbols.find_all do |cell|
+      if cell == 'O'
+        o_symbols +=1
+      end
+    end
+    expect(o_symbols).to eq(10)
+  end
+
 end
