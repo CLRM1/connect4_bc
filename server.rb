c0f037f1953bcb7234b8daf7c24c4df79367f898
Chris Romero <chrisleeromero1@gmail.com>
2022-02-14 14:00:34 -0700

    Test: all rspec tests complete and passing

--- server.rb
+++ server.rb
@@ -1,7 +1,14 @@
 # Library that contains TCPServer
-require './runner'
+# require './runner'
 require 'socket'
+require './lib/board'
+require './lib/player'
+require './lib/space'
+require './lib/computer'
+require './lib/win_checker'
 
+answer = rand(0..100)
+guess = nil
 # Create a new instance of class TCPServer on Port 9292
 
 server = TCPServer.new(9292)
@@ -12,25 +19,82 @@ loop do
   puts 'Waiting for Request'
   connection = server.accept
 
-  # Read the request line by line until we have read every line
-  puts "Got this Request:"
-  request_lines = []
-  line = connection.gets.chomp
-  while !line.empty?
-    request_lines << line
-    line = connection.gets.chomp
-  end
 
-  # Print out the Request
-  puts request_lines
-
+#experiment1 with runner file
   # Generate the Response
   puts "Sending response."
-  output = "<html>Hello from the Server side!</html>"
-  # File.open('runner.rb', 'r')
-  # output = send_file(/lib/runner)
+  connect4_game = "<html>
+    #{
+    game_count = 0
+    answer = "p or q"
+    while game_count >= 0
+      board = Board.new
+
+      player1 = Player.new(board)
+
+      computer = Computer.new(board)
+
+      win_checker = WinChecker.new(board, player1, computer)
+
+      turn_counter = 0
+      puts "Welcome to Connect 4!"
+      puts " "
+      answer = board.menu
+      if answer.upcase == 'P'
+        puts " "
+        puts "Choose a column (ABCDEFG)"
+        board.print_board
+        until player1.has_won? || computer.has_won? || win_checker.is_a_draw? do
+
+          player1.add_piece(gets.chomp)
+          computer.add_piece
+
+          turn_counter += 1
+          puts " "
+          puts "Round:#{turn_counter}"
+          puts " "
+          board.print_board
+          win_checker.check_diagonal_wins
+          win_checker.check_vertical_wins
+          win_checker.check_horizontal_wins
+        end
+
+        5.times do
+          puts " "
+        end
+        game_count += 1
+      elsif answer.upcase == 'Q'
+        puts "See you later!"
+        break
+      end
+
+    end
+  }
+  </html>"
   status = "http/1.1 200 ok"
-  response = status + "\r\n" + "\r\n" + output
+  response = status + "\r\n" + "\r\n" + connect4_game
+#---end experiment1
+
+# Read the request line by line until we have read every line
+puts "Got this Request:"
+request_lines = []
+request_lines << connect4_game 
+line = connection.gets.chomp
+while !line.empty?
+  request_lines << line
+  line = connection.gets.chomp
+end
+
+# Print out the Request
+puts request_lines
+
+# Extract the guess parameter if the Request included a guess
+request_line = request_lines[0]
+if request_line.include? '?'
+  path = request_line.split[1]
+  params = path.split("guess=")
+  guess = params[-1].to_i
+end
 
   # Send the Response
   connection.puts response
--- spec/win_checker_spec.rb
+++ spec/win_checker_spec.rb
@@ -72,7 +72,11 @@ describe WinChecker do
     3.times do
       user_player.add_piece('C')
     end
-    4.times do
+    board.rows[:row6][3].symbol = 'O'
+    board.rows[:row6][3].is_empty = false
+
+    # require 'pry'; binding.pry
+    3.times do
       user_player.add_piece('D')
     end
     spot_1 = user_player.board.rows[:row6][0]
@@ -85,6 +89,7 @@ describe WinChecker do
     expect(spot_4.symbol).to eq('X')
     game_status.check_diagonal_wins
     expect(user_player.has_won).to be(true)
+
   end
 
   it "can determine a draw" do
@@ -92,15 +97,73 @@ describe WinChecker do
     user_player = Player.new(board)
     computer = Computer.new(board)
     game_status = WinChecker.new(board, user_player, computer)
-    6.times do
+    3.times do
       user_player.add_piece('A')
       user_player.add_piece('B')
-      user_player.add_piece('C')
       user_player.add_piece('D')
+    end
+
+    board.rows[:row3][0].symbol = 'O'
+    board.rows[:row3][0].is_empty = false
+    board.rows[:row3][1].symbol = 'O'
+    board.rows[:row3][1].is_empty = false
+    board.rows[:row2][0].symbol = 'O'
+    board.rows[:row2][0].is_empty = false
+    board.rows[:row2][1].symbol = 'O'
+    board.rows[:row2][1].is_empty = false
+    board.rows[:row1][0].symbol = 'O'
+    board.rows[:row1][0].is_empty = false
+    board.rows[:row1][1].symbol = 'O'
+    board.rows[:row1][1].is_empty = false
+    board.rows[:row6][2].symbol = 'O'
+    board.rows[:row6][2].is_empty = false
+    board.rows[:row5][2].symbol = 'O'
+    board.rows[:row5][2].is_empty = false
+    board.rows[:row4][2].symbol = 'O'
+    board.rows[:row4][2].is_empty = false
+
+    3.times do
+      user_player.add_piece('C')
+    end
+
+    board.rows[:row3][3].symbol = 'O'
+    board.rows[:row3][3].is_empty = false
+    board.rows[:row2][3].symbol = 'O'
+    board.rows[:row2][3].is_empty = false
+    board.rows[:row1][3].symbol = 'O'
+    board.rows[:row1][3].is_empty = false
+    board.rows[:row6][4].symbol = 'O'
+    board.rows[:row6][4].is_empty = false
+    board.rows[:row5][4].symbol = 'O'
+    board.rows[:row5][4].is_empty = false
+    board.rows[:row4][4].symbol = 'O'
+    board.rows[:row4][4].is_empty = false
+
+    3.times do
       user_player.add_piece('E')
       user_player.add_piece('F')
+    end
+
+    board.rows[:row3][5].symbol = 'O'
+    board.rows[:row3][5].is_empty = false
+    board.rows[:row2][5].symbol = 'O'
+    board.rows[:row2][5].is_empty = false
+    board.rows[:row1][5].symbol = 'O'
+    board.rows[:row1][5].is_empty = false
+    board.rows[:row6][6].symbol = 'O'
+    board.rows[:row6][6].is_empty = false
+    board.rows[:row5][6].symbol = 'O'
+    board.rows[:row5][6].is_empty = false
+    board.rows[:row4][6].symbol = 'O'
+    board.rows[:row4][6].is_empty = false
+
+    3.times do
       user_player.add_piece('G')
     end
+
+    game_status.check_diagonal_wins
+    game_status.check_vertical_wins
+    game_status.check_horizontal_wins
     expect(game_status.is_a_draw?).to eq("It's a draw!!")
     end
 
